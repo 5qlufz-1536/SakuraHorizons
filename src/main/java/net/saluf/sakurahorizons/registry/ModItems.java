b9e978f18d85513fb2b3cb9894e2679e4db01a87
nb000o <kanzume9630@gmail.com>
2025-03-21 23:23:07 +0900

    feat: 塩の追加

--- src/main/java/net/saluf/sakurahorizons/registry/ModItems.java
+++ src/main/java/net/saluf/sakurahorizons/registry/ModItems.java
@@ -26,7 +26,8 @@ public class ModItems {
     public static final Item EXAMPLE_ITEM = registerItem("onigiri", settings -> new Item(settings.food(new FoodComponent.Builder().nutrition(4).saturationModifier(0.3f).build())), new Item.Settings());
     public static final Item TOFU = registerItem("tofu", settings -> new Item(settings.food(new FoodComponent.Builder().nutrition(2).saturationModifier(0.3f).build())), new Item.Settings());
     public static final Item SHAKE_ONIGIRI = registerItem("shake_onigiri", settings -> new Item(settings.food(new FoodComponent.Builder().nutrition(6).saturationModifier(0.3f).build())), new Item.Settings());
-    public static final Item NAGINATA = registerItem("naginata",settings -> new SwordItem(GUIDITE_TOOL_MATERIAL,1f,0.9f,settings),new Item.Settings());
+    public static final Item NAGINATA = registerItem("naginata",settings -> new SwordItem(GUIDITE_TOOL_MATERIAL,1f,1f,settings),new Item.Settings());
+    public static final Item SALT = registerItem("salt",Item::new, new Item.Settings());
     //public static final Item EXAMPLE_ITEM = registerItem("onigiri", OnigiriItem::new, new Item.Settings());
 
     private static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
@@ -42,6 +43,9 @@ public class ModItems {
         });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
             entries.add(NAGINATA);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SALT);
         });
     }
 }
