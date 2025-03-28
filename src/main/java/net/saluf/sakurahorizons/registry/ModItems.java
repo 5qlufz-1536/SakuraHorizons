41d4e650175ab7c7b6725ed4e27c270bb2e923de
5z-lql2 <5zabc4989@gmail.com>
2025-03-29 05:46:08 +0900

    fix: 茶葉を設置可能にしたよ

--- src/main/java/net/saluf/sakurahorizons/registry/ModItems.java
+++ src/main/java/net/saluf/sakurahorizons/registry/ModItems.java
@@ -40,8 +40,8 @@ public class ModItems {
     public static final Item YAKI_ONIGIRI = registerItem("yaki_onigiri",settings -> new Item(settings.food(new FoodComponent.Builder().nutrition(5).saturationModifier(0.3f).build())), new Item.Settings());
     public static final Item SHOYU = registerItem("shoyu",Item::new, new Item.Settings().maxCount(16));
     public static final Item KATSUO = registerItem("katsuo",settings -> new Item(settings.food(new FoodComponent.Builder().nutrition(1).saturationModifier(0.3f).build())), new Item.Settings());
-    public static final Item TYABA = registerItem("tyaba", Item::new, new Item.Settings());
-    //public static final Item EXAMPLE_ITEM = registerItem("onigiri", OnigiriItem::new, new Item.Settings());
+    public static final Item TYABA = registerItem("tyaba", settings -> new BlockItem(ModBlocks.GREENTEA_BUSH_BLOCK, settings), new Item.Settings());
+    //public static final Item EXAMPLE_ITEM = registerItem("onigiri", (Function<Item.Settings, Item>) new BlockItem(ModBlocks.RICE_CROP_BLOCK, new Item.Settings()), new Item.Settings());
 
     private static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("sakurahorizons", name));
