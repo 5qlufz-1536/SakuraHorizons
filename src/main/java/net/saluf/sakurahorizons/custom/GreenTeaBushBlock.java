b416ec1fbb4c531241f78e06c4f7866fde879a02
5z-lql2 <5zabc4989@gmail.com>
2025-03-29 03:34:02 +0900

    feat: お茶用クラス追加

---
+++ src/main/java/net/saluf/sakurahorizons/custom/GreenTeaBushBlock.java
@@ -0,0 +1,9 @@
+package net.saluf.sakurahorizons.custom;
+
+import net.minecraft.block.SweetBerryBushBlock;
+
+public class GreenTeaBushBlock extends SweetBerryBushBlock {
+    public GreenTeaBushBlock(Settings settings) {
+        super(settings);
+    }
+}
